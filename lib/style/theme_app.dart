5016f3e9b37f415fd30edd6db2479e4f63e45a7d
Jacob Larte <robsonpereirawill@gmail.com>
2022-12-24 15:00:00 +0100

    primeiro commit

---
+++ lib/style/theme_app.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/animation.dart';
+
+class ThemeApp {
+  static const Color colorButton = Color(0xFF21899C);
+  static const Color colorButtonSocial = Color(0xFF134140);
+  static const Color passwordTextField = Color(0xFF15224F);
+}
